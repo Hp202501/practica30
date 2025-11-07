e174050622d0876f4f525ee2f0d6a17c142f2ae7
Hp202501 <Hp202501@upes.edu.sv>
2025-11-07 06:51:30 -0600

    error 2307

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'vehiculos', loadComponent: () => import('./pages/vehiculos/vehiculos.component').then(m => m.VehiculosComponent) },
+  { path: 'clientes', loadComponent: () => import('./pages/clientes/clientes.component').then(m => m.ClientesComponent) },
+  { path: 'mecanicos', loadComponent: () => import('./pages/mecanicos/mecanicos.component').then(m => m.MecanicosComponent) },
+  { path: 'servicios', loadComponent: () => import('./pages/servicios/servicios.component').then(m => m.ServiciosComponent) },
+  { path: '**', redirectTo: 'home' }
+];
